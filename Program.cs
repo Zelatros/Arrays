cc65ded4fbe3d1ecebeed86f1b6933b17e2ab10a
TTuna <t.tunakoseler@gmail.com>
2022-12-19 23:39:38 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        // dizi tanımlama
+        string[] renkler = new string[5];
+
+        string[] hayvanlar = {"kedi", "köpek", "kuş", "maymun"};
+
+        int[] dizi;
+        dizi = new int[5];
+
+        //dizilere değer atama ve erişim
+        renkler[0] = "mavi";
+        dizi[3] = 10;
+        
+        Console.WriteLine(hayvanlar[1]);
+        Console.WriteLine(dizi[3]);
+        Console.WriteLine(renkler[0]);
+
+        //döngülerle dizi kullanımı
+        //klavyeden girilen n tane sayının ortalaması
+
+        Console.Write("Lüften dizinin eleman sayısını giriniz: ");
+        int arrlength = int.Parse(Console.ReadLine());
+        int[] intArr = new int[arrlength];
+        int sum = 0;
+
+        for (int i = 0; i < arrlength; i++)
+        {
+            Console.Write("Lütfen {0}. elemanı giriniz: ", i+1);
+            intArr[i] = int.Parse(Console.ReadLine());
+        }
+
+        foreach (var number in intArr)
+        {
+            sum += number;
+        }
+
+        Console.WriteLine(sum/arrlength);
+
+    }
+}
---
+++ README.md
@@ -0,0 +1,3 @@
+# Arrays (www.patika.dev)
+
+---
